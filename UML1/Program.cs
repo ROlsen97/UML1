bd979edc25fa54b598b84b275308176f3993b8dd
Bruger <Bruger@LAPTOP-LHCL8MS0>
2022-03-02 15:45:27 +0100

    Add project files.

---
+++ UML1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UML1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OMFG, you are yeezys christ!");
+        }
+    }
+}
---
+++ UML1/Store.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UML1
+{
+    class Store
+    {
+        public void start()
+        {
+            Pizza p1 = new Pizza();
+            Pizza p2 = new Pizza();
+            Pizza p3 = new Pizza();
+
+            Customer c1 = new Customer();
+            Customer c2 = new Customer();
+            Customer c3 = new Customer();
+
+            Order o1 = new Order(p1);
+            Order o2 = new Order();
+            Order o3 = new Order();
+
+            Console.WriteLine(o1);
+        }
+    }
+}
